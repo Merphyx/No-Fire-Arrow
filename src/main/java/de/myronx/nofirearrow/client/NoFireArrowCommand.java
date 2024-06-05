91e61f43dba28f95d4d3c10182fcb61ff96b7c59
Merphyx <merphyx@gmail.com>
2024-06-05 20:32:56 +0200

    1.1

--- src/main/java/de/myronx/nofirearrow/client/NoFireArrowCommand.java
+++ src/main/java/de/myronx/nofirearrow/client/NoFireArrowCommand.java
@@ -10,12 +10,23 @@ public class NoFireArrowCommand extends NoFireArrowClient {
     public static void register() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             dispatcher.register(ClientCommandManager.literal("nfa")
-                    .executes(context -> {
-                        togglefire = !togglefire;
-                        Text message = togglefire ? Text.literal("§7Feuerpfeile: §cdeaktiviert") : Text.literal("§7Feuerpfeile: §aaktiviert");
-                        MinecraftClient.getInstance().inGameHud.setOverlayMessage(message, false);
-                        return 1;
-                    }));
+                    .then(ClientCommandManager.literal("nofire")
+                            .executes(context -> {
+                                toggleFire = !toggleFire;
+                                Text message = toggleFire ? Text.literal("§7Feuerpfeile: §cdeaktiviert") : Text.literal("§7Feuerpfeile: §aaktiviert");
+                                MinecraftClient.getInstance().inGameHud.setOverlayMessage(message, false);
+                                return 1;
+                            })
+                    )
+                    .then(ClientCommandManager.literal("render")
+                            .executes(context -> {
+                                renderArrow = !renderArrow;
+                                Text message = renderArrow ? Text.literal("§7Pfeile: §aaktiviert") : Text.literal("§7Pfeile: §cdeaktiviert");
+                                MinecraftClient.getInstance().inGameHud.setOverlayMessage(message, false);
+                                return 1;
+                            })
+                    )
+            );
         });
     }
 }
